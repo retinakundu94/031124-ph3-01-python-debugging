4ea83f93f4f437e788f179986c910d1e6828bf79
brewchetta <chett.tiller@gmail.com>
2024-03-18 12:35:48 -0400

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Python Fundamentals and Debugging
+
+## Learning Goals
+
+- Running python files with `python3` and `python3 -i`
+
+- Understand differences between Python data types
+
+- Installing and importing packages with `pip`
+
+- Using `ipdb` and `print` to debug issues
+
+- Python functions and function scope
+
+## Getting Started
+
+Fork and clone this repository.
+
+You can add `ipdb` to your local `pip` with `pip install ipdb`.
+
+You can run the `main.py` file with `python3 main.py`.
---
+++ main.py
@@ -0,0 +1,31 @@
+# --- SYNTAX ERROR --- #
+
+def add(param1:int, param2:int):
+return param1 + param2
+
+
+# --- TYPE ERROR --- #
+
+# my_string = "6"
+
+# my_number = 5
+
+# def add(param1:int, param2:int):
+#     return param1 + param2
+
+# add(my_string + my_number)
+
+# --- INFINITE LOOP --- #
+
+# i = 0
+# while i < 10:
+#     print(i)
+
+# --- COMPARISONS (TYPE ERROR) --- #
+
+# if my_string > my_number:
+#     print(f"{my_string} is greater than {my_number}")
+# elif my_number > my_string:
+#     print(f"{my_number} is greater than {my_string}")
+# else:
+#     print("I think they're the same number")
